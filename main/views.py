28f9202159e017beef88e4983f330989ae206453
topahilangharapan <musthofa.joko@ui.ac.id>
2023-09-06 15:17:27 +0700

    add: Model-View-Template Django

---
+++ main/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+
+# Create your views here.
+def show_main(request):
+    context = {
+        'name': 'Musthofa Joko Anggoro',
+        'class': 'PBP E'
+    }
+
+    return render(request, "main.html", context)
